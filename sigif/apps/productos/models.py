6230905ee5925e51e963c6377eb562d4b59e725a
juanfe1923 <felipeuribe778@gmail.com>
2026-03-27 15:52:50 -0500

    cambios models

--- sigif/apps/productos/models.py
+++ sigif/apps/productos/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Producto (models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True, null=True)
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    precio = models.DecimalField(max_digits=100, decimal_places=0)
     stock = models.IntegerField(default=0)
     categoria = models.CharField(max_length=100)
     activo = models.BooleanField(default=True)
