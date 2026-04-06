e01102f054532421ecd81a666026030dc77bf3df
DMoya-max <dmoya1873@gmail.com>
2026-04-05 19:49:00 -0500

    cambios aplicados

--- sigif/apps/usuarios/urls.py
+++ sigif/apps/usuarios/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.login_view, name='login'),
-    path('us/', views.usuarios, name='usuarios'),
+    path('usuarios/', views.usuarios, name='usuarios'),
     path('crear_usuarios/', views.crear_usuarios, name='crear_usuarios'),
-    path('listar_usuarios/', views.listar_usuarios, name='listar_usuarios'),
     path('editar_usuarios/<int:id>/', views.editar_usuarios, name='editar_usuarios'),
     path('eliminar_usuario/<int:id>/', views.eliminar_usuario, name='eliminar_usuario'),
 ]
--- sigif/apps/usuarios/views.py
+++ sigif/apps/usuarios/views.py
@@ -19,21 +19,20 @@ def login_view(request):
     return render(request, "usuarios/login.html")
 
 def usuarios(request):
-    return render(request, "usuarios/usuarios.html")
+    users = Usuarios.objects.all()
+    return render(request, 'usuarios/usuarios.html', {'users': users})
+    
 
 def crear_usuarios(request):
     if request.method == 'POST':
         form = UsuarioForm (request.POST)
         if form.is_valid():
             form.save()
-            return redirect('listar_usuarios')
+            return redirect('usuarios')
     else:
         form = UsuarioForm ()
     return render(request, "usuarios/crear_usuarios.html",  {'form': form})
 
-def listar_usuarios(request):
-    users = Usuarios.objects.all()
-    return render(request, 'usuarios/listar_usuarios.html', {'users': users})
 
 def editar_usuarios(request, id):
     user = get_object_or_404(Usuarios, id=id)
@@ -41,7 +40,7 @@ def editar_usuarios(request, id):
         form = UsuarioForm(request.POST, instance=user)
         if  form.is_valid():
             form.save()
-            return redirect('listar_usuarios')
+            return redirect('usuarios')
     else:
         form = UsuarioForm(instance=user)
     return render(request, 'usuarios/editar_usuarios.html', {'form': form})
@@ -50,6 +49,6 @@ def eliminar_usuario(request, id):
     user = get_object_or_404(Usuarios, id=id)
     if request.method == 'POST':
         user.delete()
-        return redirect('listar_usuarios')
+        return redirect('usuarios')
     else:
         return render(request, 'usuarios/eliminar_usuario.html', {'user': user})
